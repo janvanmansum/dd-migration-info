f6a7a8e5b5d2a1f82dd0dd8b918fefdaaa187552
Jan van Mansum <janvanmansum@users.noreply.github.com>
2021-06-08 17:57:36 +0200

    Fixed syntax error in db-tables.sql

--- src/main/assembly/dist/install/db-tables.sql
+++ src/main/assembly/dist/install/db-tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS basic_file_metadata (
     file_name VARCHAR(1000) NOT NULL,
     directory_label VARCHAR(1000),
     mime_type VARCHAR(255) NOT NULL,
-    sha1_checksum CHAR(40) NOT NULL
+    sha1_checksum CHAR(40) NOT NULL,
     PRIMARY KEY (storage_identifier, dataset_doi, version_sequence_number));
 
 GRANT INSERT, SELECT, UPDATE, DELETE ON basic_file_metadata TO dd_migration_info;
